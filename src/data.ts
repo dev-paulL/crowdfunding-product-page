395fce8ee7e4df63c2a5470f68ba8f408b7890e8
Paul L. <13600558+dev-paulL@users.noreply.github.com>
2024-09-15 15:49:17 +0200

    added pledgePlans constant

---
+++ src/data.ts
@@ -0,0 +1,31 @@
+type pledgePlan = {
+    name:"Bamboo Stand" | "Black Edition Stand" | "Mahogany Special Edition",
+    pledgeAmount:number,
+    description:string,
+    amountInStock:number
+}
+
+export const pledgePlans : pledgePlan[] = [
+  {
+    name: "Bamboo Stand",
+    pledgeAmount: 25,
+    description:
+      "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list.",
+    amountInStock: 101,
+  },
+  {
+    name: "Black Edition Stand",
+    pledgeAmount: 75,
+    description:
+      "You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
+    amountInStock: 64,
+  },
+  {
+    name:'Mahogany Special Edition',
+        pledgeAmount: 200,
+        description:"You get two Special Edition Mahogany stands, a Back T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
+        amountInStock:0,
+  }
+];
+
+export const amountAimed = 100000;
